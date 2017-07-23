9f02010699f43f2a0e6b24fb67dc789bb3d9217f
Kanra153 <mad_hatter153@bk.ru>
2017-07-24 02:27:24 +0300

    Первый коммит

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,19 @@
 import json
-
+import sys 
 
 def load_data(filepath):
-    pass
-
+	with open(filepath, 'r') as raw_json:
+		data = json.load(raw_json)
+	return data	
 
 def pretty_print_json(data):
-    pass
+	pprint = json.dumps(data, ensure_ascii=False, indent=3)
+	return pprint
 
 
 if __name__ == '__main__':
-    pass
+	filepath = sys.argv[1]
+	data = load_data(filepath)
+	pprint = pretty_print_json(data)
+	print('Структурированный json:')
+	print(pprint)

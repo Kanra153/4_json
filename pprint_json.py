673dfffb26440bfef02327b6473c0b9890f678bf
Kanra153 <mad_hatter153@bk.ru>
2017-07-26 15:09:05 +0300

    Поправил отступы, исправил названия переменных

--- pprint_json.py
+++ pprint_json.py
@@ -3,24 +3,25 @@ import sys
 import os 
 
 def load_data(filepath):
-	if not os.path.exists(filepath):
-		print('Вы ввели неправильный путь до файла')
-		return None
-	with open(filepath, 'r') as raw_json:
-		data = json.load(raw_json)
-	return data	
+    if not os.path.exists(filepath):
+    print('Вы ввели неправильный путь до файла')
+    return None
+	with open(filepath, 'r') as file_handler:
+    raw_json = json.load(file_handler)
+    return raw_json
 
-def pretty_print_json(data):
-	pprint = json.dumps(data, ensure_ascii=False, indent=4)
-	return pprint
+def pretty_print_json(raw_json):
+    pprint = json.dumps(raw_json, ensure_ascii=False, indent=3)
+    return pprint
 
 if __name__ == '__main__':
-	if len(sys.argv)>1:
-			filepath = sys.argv[1]
-			data = load_data(filepath)
-			pprint = pretty_print_json(data)
-			print('Структурированный json:')
-			print(pprint)
+    if len(sys.argv)>1:
+            filepath = sys.argv[1]
+            raw_json = load_data(filepath)
+            pprint = pretty_print_json(raw_json)
+            print('Структурированный json:')
+            print(pprint)
 	else: 
-		print('Вы не ввели путь до файла')		
+        print('Вы не ввели путь до файла')		
 	 	
+

5f952ec411f77df11b6826687f5a523433f875c0
Kanra153 <mad_hatter153@bk.ru>
2017-12-15 11:32:03 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -5,21 +5,20 @@ import os
 def load_data(filepath):
     if not os.path.exists(filepath):
         print('Вы ввели неправильный путь до файла')
-        return None
+        exit()
     with open(filepath, 'r') as file_handler:
         raw_json = json.load(file_handler)
     return raw_json
 
-def pretty_print_json(raw_json):
-    pprint = json.dumps(raw_json, ensure_ascii=False, indent=3)
-    return pprint
+def prettify_json(raw_json):
+    pretty_json = json.dumps(raw_json, ensure_ascii=False, indent=3)
+    return pretty_json
 
 if __name__ == '__main__':
     if len(sys.argv)>1:
             filepath = sys.argv[1]
             raw_json = load_data(filepath)
-            pprint = pretty_print_json(raw_json)
-            print('Структурированный json:')
-            print(pprint)
+            pprint = prettify_json(raw_json)
+            print('Структурированный json: ', '\n', pprint)
     else: 
-        print('Вы не ввели путь до файла')		
+        print('Вы не ввели путь до файла')  

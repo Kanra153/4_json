eaaa64e8f29bc179c99a86cb246aaf4b8ee01f8b
Kanra153 <mad_hatter153@bk.ru>
2017-12-15 12:10:40 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -2,17 +2,20 @@ import json
 import sys
 import os 
 
+
 def load_data(filepath):
     if not os.path.exists(filepath):
         print('Вы ввели неправильный путь до файла')
-        exit()
+        return None
     with open(filepath, 'r') as file_handler:
         raw_json = json.load(file_handler)
     return raw_json
 
+
 def prettify_json(raw_json):
     pretty_json = json.dumps(raw_json, ensure_ascii=False, indent=3)
     return pretty_json
+
 
 if __name__ == '__main__':
     if len(sys.argv)>1:

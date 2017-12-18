a773bce51fa1478aaca4d0f88a833a244bf16fab
Kanra153 <mad_hatter153@bk.ru>
2017-12-18 19:55:18 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,21 +1,22 @@
 import json
 import os
+import sys
 
 
 def load_data(file_path):
     with open(file_path, 'r') as file_handler:
-        raw_json = json.load(file_handler)
-    return raw_json
+        parsed_json = json.load(file_handler)
+    return parsed_json
 
 
-def pprint_json(raw_json):
-    print(json.dumps(raw_json, ensure_ascii=False, indent=3))
+def pprint_json(parsed_json):
+    print(json.dumps(parsed_json, ensure_ascii=False, indent=3))
 
 
 if __name__ == '__main__':
-    file_path = input('Введите путь до файла: ')
+    file_path = sys.argv[1]
     if os.path.exists(file_path):
-        raw_json = load_data(file_path)
-        pprint_json(raw_json)
+        parsed_json = load_data(file_path)
+        pprint_json(parsed_json)
     else:
         print('Файл не найден!')

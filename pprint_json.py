735d99ddb72f169d18c031409f5d8d618f7a98ed
Kanra153 <mad_hatter153@bk.ru>
2018-02-13 18:25:15 +0300

    Fixed encoding trouble

--- pprint_json.py
+++ pprint_json.py
@@ -4,7 +4,7 @@ import sys
 
 
 def load_data(file_path):
-    with open(file_path, 'r') as file_handler:
+    with open(file_path, 'r', encoding='utf-8') as file_handler:
         parsed_json = json.load(file_handler)
     return parsed_json
 
